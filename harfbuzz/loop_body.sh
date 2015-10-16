598f9fbeea91892b87502abd05fd0aae74eb353e
Kostya Serebryany <konstantin.s.serebryany@gmail.com>
2015-10-16 14:53:05 -0700

    more harfbuzz

--- harfbuzz/loop_body.sh
+++ harfbuzz/loop_body.sh
@@ -8,7 +8,21 @@ mkindex() {
   (cd /var/www/html/; sudo $P/mkindex.sh index.html *log)
 }
 
+dump_coverage() {
+  echo ===================================================================
+  echo ==== FUNCTION-LEVEL COVERAGE: THESE FUNCTIONS ARE *NOT* COVERED ===
+  echo ===================================================================
+  sancov.py print *sancov  2> /dev/null |\
+    sancov.py missing $1 2> /dev/null |\
+    llvm-symbolizer -obj $1 -inlining=0 -functions=none |\
+    grep harfbuzz |\
+    sed "s#.*harfbuzz/##g" |\
+    sort --field-separator=: --key=1,1 --key=2n,2 --key=3n,3 | cat -n
+}
+
 BUCKET=gs://font-fuzzing-corpora/
+CORPUS=CORPORA/C1
+MAX_LEN=2048
 export ASAN_OPTIONS=quarantine_size_mb=10 # Make asan less memory-hungry.
 J=$(grep CPU /proc/cpuinfo | wc -l )
 
@@ -26,7 +40,7 @@ $P/build.sh asan_cov -fsanitize=address -fsanitize-coverage=edge,8bit-counters >
 $P/build.sh func -fsanitize=shift -fsanitize-coverage=func > func_build.log 2>&1 &
 wait
 echo =========== FUZZING
-./harfbuzz_asan_cov_fuzzer -max_len=2048 ./CORPORA/C1  -artifact_prefix=../CORPORA/ARTIFACTS -jobs=$J -workers=$J -max_total_time=60 > $L 2>&1
+./harfbuzz_asan_cov_fuzzer -max_len=$MAX_LEN $CORPUS  -artifact_prefix=../CORPORA/ARTIFACTS -jobs=$J -workers=$J -max_total_time=60 > $L 2>&1
 exit_code=$?
 case $exit_code in
   0) prefix=pass
@@ -34,6 +48,10 @@ case $exit_code in
   *) prefix=FAIL
     ;;
 esac
+echo =========== DUMP COVERAGE
+rm -f *sancov
+UBSAN_OPTIONS=coverage=1 ./harfbuzz_func_fuzzer -max_len=$MAX_LEN $CORPUS -runs=0 > func_run.log 2>&1
+dump_coverage harfbuzz_func_fuzzer >> $L
 echo =========== SYNC CORPORA BACK
 gsutil -m rsync -r CORPORA $BUCKET/CORPORA
 echo =========== UPDATE WEB PAGE
